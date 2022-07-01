8c3e9252bb3c7ce2811b64eada10d0e3c295a678
8pro <mechackkobius@gmail.com>
2022-07-01 12:42:01 +0300

    Backend project init

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cookieParser())
+  .use(
+    cors({
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      credentials: true,
+    })
+  )
+  .use("/api/public", express.static("public"));
+
+app.get("/api/", (req, res) => {
+  res.send("Welcome ! 8pro Portfolio and Blog");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on url: http://localhost:${port}`);
+});
