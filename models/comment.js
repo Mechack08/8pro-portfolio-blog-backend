7796bf01ae226c6b9c4cd8a72792a4cba16c03c2
8pro <mechackkobius@gmail.com>
2022-07-01 14:30:05 +0300

    comment.js created and link with database field Comments

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Comment.init(
+    {
+      fullname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      comment: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Comment",
+    }
+  );
+  return Comment;
+};
