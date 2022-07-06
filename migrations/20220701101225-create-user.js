d4157f942e1d6960cb71ec815be10a5bae9cbd5a
8pro <mechackkobius@gmail.com>
2022-07-06 14:13:07 +0300

    Update User Migration by adding contrains on username and email. They must be unique

--- migrations/20220701101225-create-user.js
+++ migrations/20220701101225-create-user.js
@@ -9,10 +9,12 @@ module.exports = {
       },
       username: {
         allowNull: false,
+        unique: true,
         type: Sequelize.STRING,
       },
       email: {
         allowNull: false,
+        unique: true,
         type: Sequelize.STRING,
       },
       password: {
