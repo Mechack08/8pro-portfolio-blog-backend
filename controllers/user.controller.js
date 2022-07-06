90548236167d4fe6beda10c58c5c118a1d9abfea
8pro <mechackkobius@gmail.com>
2022-07-06 14:15:49 +0300

    Working on the login and logout functions in the controller file but also update signup function by adding Op.or to verify if username or email already exist

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 const models = require("../models");
 const asyncLib = require("async");
 const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
+const jwt = require("jsonwebtoken");
 
 const EMAIL_REGEX =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -27,13 +29,13 @@ module.exports = {
       [
         (done) => {
           models.User.findOne({
-            where: { username: username },
+            where: { [Op.or]: [{ username: username }, { email: email }] },
             attributes: ["id"],
           })
             .then((user) => done(null, user))
             .catch((e) => {
               const error = "Something went wrong, try again later.";
-              return res.status(500).json({ error, data: e.message });
+              return res.status(500).json({ error, details: e.message });
             });
         },
         (user, done) => {
@@ -58,7 +60,7 @@ module.exports = {
             .catch((e) => {
               return res.status(500).json({
                 error: "error happened, please try again later",
-                data: e.message,
+                details: e.message,
               });
             });
         },
@@ -74,5 +76,65 @@ module.exports = {
         });
       }
     );
+  },
+
+  singIn: (req, res) => {
+    const { identification, password } = req.body;
+
+    const maxAge = 3 * 24 * 60 * 60 * 1000;
+    const createToken = (id) => {
+      return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge,
+      });
+    };
+
+    if (identification === null || identification === "")
+      return res.json({ message: "username or email is required" });
+    if (password === null || password == "")
+      return res.json({ message: "password is required" });
+
+    asyncLib.waterfall(
+      [
+        (done) => {
+          models.User.findOne({
+            where: {
+              [Op.or]: [
+                { username: identification },
+                { email: identification },
+              ],
+            },
+          })
+            .then((find) => done(null, find))
+            .catch((e) => {
+              return res.status(500).json({
+                error: "Something went wrong, try again later.",
+                details: e.message,
+              });
+            });
+        },
+        (find, done) => {
+          if (!find) return res.status(404).json({ message: "user not exist" });
+
+          bcrypt.compare(password, find.password, (err, pwd) => {
+            done(null, find, pwd);
+          });
+        },
+        (find, pwd, done) => {
+          if (pwd) {
+            const token = createToken(find.id);
+            res.cookie("PortfolioAndBlog", token, { httpOnly: true, maxAge });
+            done(find);
+          } else return res.status(403).json({ message: "invalid password" });
+        },
+      ],
+      (find) => {
+        return res.status(200).json({ message: "logged in" });
+      }
+    );
+  },
+
+  signOut: (req, res) => {
+    res.cookie("PortfolioAndBlog", "", { maxAge: 1 });
+    res.redirect("/");
   },
 };
