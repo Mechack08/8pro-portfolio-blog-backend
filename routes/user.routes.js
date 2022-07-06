951549be5210140944de28466a672b2b844876f1
8pro <mechackkobius@gmail.com>
2022-07-06 14:16:53 +0300

    Adding signin and signout routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,4 +4,10 @@ const userController = require("../controllers/user.controller");
 /* @Signup | http://localhost:3001/api/account/signup */
 router.route("/signup").post(userController.signUp);
 
+/* @Signin | http://localhost:3001/api/account/signin */
+router.route("/signin").post(userController.singIn);
+
+/* @Signin | http://localhost:3001/api/account/signout */
+router.route("/signout").post(userController.signOut);
+
 module.exports = router;
