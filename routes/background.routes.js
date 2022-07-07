392bcea79f248e84c9dfa4cd1f58893a5db88a13
8pro <mechackkobius@gmail.com>
2022-07-07 12:23:46 +0300

    background routes done for CRUD

--- routes/background.routes.js
+++ routes/background.routes.js
@@ -5,4 +5,13 @@ const { requireAuth } = require("../utils/jwt.utils");
 /* @Create a new Background | http://localhost:3001/api/background */
 router.route("/background").post(requireAuth, backgroundController.create);
 
+/* @Get all | http://localhost:3001/api/backgrounds */
+router.route("/backgrounds").get(backgroundController.all);
+
+/* @Update | http://localhost:3001/api/background/:id */
+router.route("/background/:id").put(backgroundController.update);
+
+/* @Update | http://localhost:3001/api/background/:id */
+router.route("/background/:id").delete(backgroundController.delete);
+
 module.exports = router;
