4afd449a5afabd7c58273e67978abcf0557026d3
8pro <mechackkobius@gmail.com>
2022-07-08 13:15:21 +0300

    article routes created

---
+++ routes/article.routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const articleController = require("../controllers/article.controller");
+const { requireAuth } = require("../utils/jwt.utils");
+
+/* @Create a new Article | http://localhost:3001/api/article */
+router
+  .route("/article")
+  .post(requireAuth, articleController.upload, articleController.create);
+
+/* @Get all | http://localhost:3001/api/articles */
+router.route("/articles").get(articleController.all);
+
+/* @Update | http://localhost:3001/api/article/:id */
+router.route("/article/:id").put(requireAuth, articleController.update);
+
+/* @Update Img | http://localhost:3001/api/article/img/:id */
+router
+  .route("/article/img/:id")
+  .put(requireAuth, articleController.upload, articleController.updateImg);
+
+/* @Update | http://localhost:3001/api/article/:id */
+router.route("/article/:id").delete(requireAuth, articleController.delete);
+
+module.exports = router;
