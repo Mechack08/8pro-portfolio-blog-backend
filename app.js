e7491d032212bd53a23db1745fe8ff2f071bf774
8pro <mechackkobius@gmail.com>
2022-07-13 18:31:10 +0300

    install mongoose dependency and update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+require("./config/mongodb.db");
 
 const app = express();
 
