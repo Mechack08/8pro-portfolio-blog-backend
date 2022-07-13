4167b9e434ac14affe391cbb25181a478e775725
8pro <mechackkobius@gmail.com>
2022-07-13 20:56:12 +0300

    new user model | mongodb

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    validate: (value) => {
+      return validator.isEmail(value);
+    },
+    lowercase: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    max: 1024,
+    minlength: 8,
+  },
+  isAdmin: {
+    type: Boolean,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
