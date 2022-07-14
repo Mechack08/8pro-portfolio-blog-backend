27e9a3c59c420d8d173891e94ffd59a74e71f101
8pro <mechackkobius@gmail.com>
2022-07-14 13:17:30 +0300

    new model file added

---
+++ models/background.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+let backgroundSchema = new mongoose.Schema({
+  institution: {
+    type: String,
+    require: true,
+  },
+  position: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Background", backgroundSchema);
