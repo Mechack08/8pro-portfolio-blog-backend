72526d48ab6a5dff030e8960883ea754a6a20125
8pro <mechackkobius@gmail.com>
2022-07-15 15:45:23 +0300

    portfolio model created with mongodb and mongoose

---
+++ models/portfolio.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const portfolioSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    ulr: {
+      type: String,
+      require: true,
+    },
+    img: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+
+module.exports = mongoose.model("Portfolio", portfolioSchema);
